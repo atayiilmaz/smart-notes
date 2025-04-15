3aa8bfa7f3fb52e0baa3e938c1760d83bdbc01f2
atayiilmaz <yiilmazataberk@gmail.com>
2025-04-15 21:39:27 +0300

    fix: configure cors

--- server/app.js
+++ server/app.js
@@ -17,7 +17,11 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    credentials: true
+
+}));
 app.use(logger('dev'));
 app.use(express.json());
 
